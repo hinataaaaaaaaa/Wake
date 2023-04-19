1535dde11721f076ac2a8c827fbe1d4cced22828
tttAlFttt <0hp3261131m102u@ezweb.ne.jp>
2023-04-19 20:14:38 +0900

    修正

--- lib/Erase_nfc.dart
+++ lib/Erase_nfc.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 
-class NFCErase{
-  nfcerase(){
+// 削除
+class NFCErase {
+  nfcerase() {
+    ValueNotifier<dynamic> result = ValueNotifier(null);
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      var ndef = Ndef.from(tag);
+      if (ndef == null || !ndef.isWritable) {
+        result.value = '消去に失敗しました';
+        NfcManager.instance.stopSession();
+        return;
+      }
 
+      String text = '';
+      NdefRecord textRecord = NdefRecord.createText(text);
+      NdefMessage message = NdefMessage([textRecord]);
+
+      await ndef.write(message);
+      //await ndef.writeLock();
+      result.value = '消去しました!"';
+      NfcManager.instance.stopSession();
+      return;
+    });
   }
 }
--- lib/Write_nfc.dart
+++ lib/Write_nfc.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 
-class NFCWrite  {
-  nfcwrite(){
+// 書き込み
+class NFCWrite {
+  nfcwrite() {
+    ValueNotifier<dynamic> result = ValueNotifier(null);
+    final TextEditingController textEditingController = TextEditingController();
+
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      var ndef = Ndef.from(tag);
+      if (ndef == null || !ndef.isWritable) {
+        result.value = '書き込み失敗しました';
+        NfcManager.instance.stopSession(errorMessage: result.value);
+        return;
+      }
+
+
+      String text = textEditingController.text;
+      NdefRecord textRecord = NdefRecord.createText(text);
+      NdefMessage message = NdefMessage([textRecord]);
+
+      try {
+        await ndef.write(message);
+        //await ndef.writeLock();
+        result.value = '書き込みました!"';
+        NfcManager.instance.stopSession();
+      } catch (e) {
+        result.value = e;
+        NfcManager.instance.stopSession(errorMessage: result.value.toString());
+        return;
+      }
+    });
   }
 }
-
